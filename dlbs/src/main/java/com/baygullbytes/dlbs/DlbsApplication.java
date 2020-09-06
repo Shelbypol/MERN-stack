3647bcc49a6100ff3c4de1c170653c8ca31b06fd
Shelby Polasek <shelbypolasek@gmail.com>
2020-09-06 15:28:38 -0500

    setup

---
+++ dlbs/src/main/java/com/baygullbytes/dlbs/DlbsApplication.java
@@ -0,0 +1,13 @@
+package com.baygullbytes.dlbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlbsApplication.class, args);
+    }
+
+}
